e11a570371fd0639f5bac40d0dc2ca272785abb0
Gray Smith <gray.smith00@gmail.com>
2018-09-26 10:49:39 -0600

    Add urlLogger and timeLogger middleware

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
+const urlLogger = (req, res, next) => {
+  console.log('Request URL:', req.url);
+  next();
+};
+
+const timeLogger = (req, res, next) => {
+  console.log('Datetime:', new Date(Date.now()).toString());
+  next();
+};
+
 app.get('/', (req, res) => {
   res.send('hello world');
+});
+
+app.get('/json', urlLogger, timeLogger, (req, res) => {
+  res.status(200).json({ name: 'Gray' });
 });
 
 app.listen(3000, () => {
