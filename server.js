0f16957e490b205dcea01c7b50c9e84c59578fee
Gray Smith <gray.smith00@gmail.com>
2018-09-30 13:23:43 -0600

    Add sunsets endpoint with image urls

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const data = require('./data');
 
+const sunsets = [
+  'https://static.rootsrated.com/image/upload/s--dOztdKo4--/t_rr_large_traditional/xeeqiz2egntxuc7u5mce.jpg',
+  'http://en.es-static.us/upl/2016/06/sunset-beach-NW-England-Adrian-Strand-lg-e1480508948619.jpg',
+  'https://static1.squarespace.com/static/51c2647ee4b097ff213b1a19/t/52599f0ce4b018d723814d2d/1381605140157/AutumnSunset3.jpg'
+];
+
 app.use(express.static('public'));
 
 const urlLogger = (req, res, next) => {
@@ -18,6 +24,10 @@ app.use(urlLogger, timeLogger);
 
 app.get('/', (req, res) => {
   res.send('hello world');
+});
+
+app.get('/sunsets', (req, res) => {
+  res.status(200).json(sunsets);
 });
 
 app.get('/json', (req, res) => {
